2fd66b17231d069c4417a6c0b45c741b79143d6b
Isla <islarcherjr@icloud.com>
2025-06-20 00:13:47 +0100

    initial commit

---
+++ src/main/java/org/project/broadcastwebsocket/BroadcastWebsocketApplication.java
@@ -0,0 +1,13 @@
+package org.project.broadcastwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroadcastWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BroadcastWebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/broadcastwebsocket/BroadcastWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.broadcastwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroadcastWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
