41ed515f6d208817bce9b636a8363f3c15476faa
Isla <islarcherjr@icloud.com>
2025-06-20 23:37:25 +0100

    got first working implementation of broadcast-websocket

---
+++ src/main/java/org/project/broadcastwebsocket/ChatHandler.java
@@ -0,0 +1,64 @@
+package org.project.broadcastwebsocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class ChatHandler extends TextWebSocketHandler {
+
+    private final Set<WebSocketSession> sessions = ConcurrentHashMap.newKeySet();
+    private final Map<WebSocketSession, String> names = new ConcurrentHashMap<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        sendMessage(session, "welcome, what shall we call you? ");
+
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+
+        if (!names.containsKey(session)) {
+            names.put(session, payload);
+            broadcast("[" + payload + "] has joined the chat.");
+        } else {
+            String name = names.get(session);
+            broadcast("[" + name + "]: " + payload);
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+    }
+
+    private void sendMessage(WebSocketSession session, String message) {
+        try {
+            session.sendMessage(new TextMessage(message));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void broadcast(String message) {
+        TextMessage textMessage = new TextMessage(message);
+
+        for (WebSocketSession session : sessions) {
+            try {
+                session.sendMessage(textMessage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
