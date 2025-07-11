dd61e75bc16071d70b6cdde3a7846fe3b6c293c8
Isla <islarcherjr@icloud.com>
2025-07-11 23:08:04 +0100

    update: bare websocket config files

---
+++ src/main/java/org/project/broadcastwebsocket/controller/ChatController.java
@@ -0,0 +1,11 @@
+package org.project.broadcastwebsocket.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@MessageMapping("")
+@SendTo("")
+public class ChatController {
+}
--- src/main/java/org/project/broadcastwebsocket/websocket/WebSocketConfig.java
+++ src/main/java/org/project/broadcastwebsocket/websocket/WebSocketConfig.java
@@ -1,21 +1,27 @@
 package org.project.broadcastwebsocket.websocket;
 
 import lombok.AllArgsConstructor;
-import org.project.broadcastwebsocket.config.ChatHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
+@EnableWebSocketMessageBroker
 @AllArgsConstructor
-public class WebSocketConfig implements WebSocketConfigurer {
-
-    private final ChatHandler chatHandler;
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatHandler, "/chat").setAllowedOrigins("*");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("");
+        registry.enableSimpleBroker("");
     }
 }
